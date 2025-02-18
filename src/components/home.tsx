e1bbbd1076fc48b21d29366b2347c3742a8aa2fb
Kaempy <campa.banj@gmail.com>
2025-02-18 18:26:43 +0100

    chore: fb

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -34,7 +34,7 @@ const Home = () => {
         }}
       />
       <footer className="flex flex-col xl:flex-row gap-8 lg:gap-0 relative items-end justify-between">
-        <p className="xl:font-medium font-semibold text-[1.75rem] leading-9 md:text-4xl xl:leading-[5.0313rem] xl:text-[4.375rem] w-full max-w-[450px] xl:max-w-[59.375rem]">
+        <p className="font-medium text-[3.125rem] leading-[3.5625rem] md:text-4xl xl:leading-[5.0313rem] xl:text-[4.375rem] w-full max-w-[450px] xl:max-w-[59.375rem]">
           The Art of Transforming Nature into Legacies
         </p>
         <p className="uppercase text-[.625rem] w-full max-w-[10rem] leading-[.9375rem] text-right">
